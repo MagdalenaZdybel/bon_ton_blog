cb8effe0147ca57345b2306c93652747f46d3f6d
Magda <magda@motta.com.pl>
2017-02-16 20:18:09 +0100

    utworzenie bazy danych

---
+++ baza danych/magda_blog.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 16 Lut 2017, 20:16
+-- Wersja serwera: 10.1.21-MariaDB
+-- Wersja PHP: 7.1.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Baza danych: `magda_blog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `wpisy`
+--
+
+CREATE TABLE `wpisy` (
+  `ID` int(11) NOT NULL,
+  `tytuł` varchar(256) COLLATE utf8_polish_ci NOT NULL,
+  `opis` varchar(256) COLLATE utf8_polish_ci NOT NULL,
+  `tresc` text COLLATE utf8_polish_ci NOT NULL,
+  `data` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `slowakluczowe` varchar(256) COLLATE utf8_polish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indexes for table `wpisy`
+--
+ALTER TABLE `wpisy`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `wpisy`
+--
+ALTER TABLE `wpisy`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
